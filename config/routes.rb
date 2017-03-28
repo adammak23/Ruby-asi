52459fc82fad38b1402e2202830bb4142b36659f
adammak2342 <adammak2342@users.noreply.github.com>
2017-03-28 10:45:08 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :articles do
     resources :comments
   end
-	get 'welcome' => 'welcome#index' #not needed
-	get 'homes/show' => "homes/show"
+#	get 'welcome' => 'welcome#index' #not needed
+	get 'homes/show' => "homes#show"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
